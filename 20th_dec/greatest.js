f31efe9aa9e21b8ef6fc012fb43cb97acbf75c57
onkarchaudhari1999 <94893796+onkarchaudhari1999@users.noreply.github.com>
2022-01-02 10:06:07 +0530

    Add files via upload

---
+++ 20th_dec/greatest.js
@@ -0,0 +1,16 @@
+var n1= 100;
+var n2= 77;
+var n3= 50;
+
+if(n1>n2 && n1>n3){
+console.log("N1 is greatest number")
+console.log(n1);
+}
+if(n2>n1 && n2>n3){
+console.log("N2 is greatest number")
+console.log(n2);
+}
+if(n3>n1 && n3>n2){
+console.log("N3 is greatest number")
+console.log(n3);
+}
