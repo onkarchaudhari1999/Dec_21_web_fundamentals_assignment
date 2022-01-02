a8049336eab7bf401ffd0aa2b03ac5bf974767e2
onkarchaudhari1999 <94893796+onkarchaudhari1999@users.noreply.github.com>
2022-01-02 10:25:45 +0530

    Add files via upload

---
+++ 22nd_Dec_Assignment/array_max.js
@@ -0,0 +1,13 @@
+var a = [];
+var b = prompt("Enter 1st number");
+b = parseInt(b);
+a.push(b);
+b = prompt("Enter 2nd number");
+b = parseInt(b);
+a.push(b);
+b = prompt("Enter 3rd number");
+b = parseInt(b);
+a.push(b);
+console.log(a);
+
+alert("maximum is " + Math.max(...a))
