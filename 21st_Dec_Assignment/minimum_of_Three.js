cf48d59fa2211dc42d2ad3ee68bfcdfa183cd0fc
onkarchaudhari1999 <94893796+onkarchaudhari1999@users.noreply.github.com>
2022-01-02 10:10:24 +0530

    Add files via upload

---
+++ 21st_Dec_Assignment/minimum_of_Three.js
@@ -0,0 +1,16 @@
+var n1 = prompt("Enter Number 1");
+var n2 = prompt("Enter Number 2");
+var n3 = prompt("Enter Number 3");
+ 
+if((n1<n2) && (n1<n3)){
+alert("Number 1 is smallest number");
+
+}
+else if((n2<n1) && (n2<n3)){
+alert("Number 2 is smallest number");
+
+}
+else if((n3<n1) && (n3<n2)){
+alert("Number 3 is smallest number");
+
+}
---
+++ 21st_Dec_Assignment/vowel_or_not.js
@@ -0,0 +1,20 @@
+var alphabate = prompt("Enter the character");
+
+if((alphabate == "a") || (alphabate == "A")){
+   alert("Given character is Vowel");
+}
+else if((alphabate == "e") || (alphabate == "E")){
+   alert("Given character is Vowel");
+}
+else if((alphabate == "i") || (alphabate == "I")){
+   alert("Given character is Vowel");
+}
+else if((alphabate == "o") || (alphabate == "O")){
+   alert("Given character is Vowel");
+}
+else if((alphabate == "u") || (alphabate == "U")){
+   alert("Given character is Vowel");
+}
+else{
+   alert("Given character is consonant");
+}
